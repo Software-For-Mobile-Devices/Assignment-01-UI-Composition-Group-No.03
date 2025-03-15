a49582f4c568120e064390d6f74e084e29780f0f
Zainab Talha <133660467+Zainabtalha@users.noreply.github.com>
2025-03-16 02:32:11 +0500

    Theme of Big Card update

--- Task_01/flutter_application_1/lib/main.dart
+++ Task_01/flutter_application_1/lib/main.dart
@@ -17,7 +17,8 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 208, 106, 188)),
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 208, 106, 188)),
         ),
         home: MyHomePage(),
       ),
@@ -31,20 +32,19 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
-
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-      var pair = appState.current;  
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
           Text('A random AWESOME idea:'),
-          Text(pair.asLowerCase),  
+          Text(pair.asLowerCase),
           ElevatedButton(
             onPressed: () {
               print('button pressed!');
@@ -67,6 +67,14 @@ class BigCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(pair.asLowerCase);
+    final theme = Theme.of(context); // ← Add this.
+
+    return Card(
+      color: theme.colorScheme.primary, // ← And also this.
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(pair.asLowerCase),
+      ),
+    );
   }
 }
